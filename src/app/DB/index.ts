64b8d097acb8409fc86792907341da1ba56cefb3
Akash Khan <mdakashkhanbdinto@gmail.com>
2025-06-09 17:10:13 +0600

    super admin some bug fixed

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,7 +1,7 @@
 import config from '../config';
 import { USER_ROLE } from '../modules/User/user.constant';
 import { User } from '../modules/User/user.model';
-
+import bcrypt from 'bcrypt';
 const superUser = {
   name: {
     firstName: 'Super',
@@ -19,6 +19,8 @@ const superUser = {
 const seedSuperAdmin = async () => {
   const isSuperAdminExists = await User.findOne({ role: USER_ROLE.superAdmin });
   if (!isSuperAdminExists) {
+     const hashedPassword = await bcrypt.hash(superUser.password ?? '', 10);
+      superUser.password = hashedPassword;
     await User.create(superUser);
   }
 };
